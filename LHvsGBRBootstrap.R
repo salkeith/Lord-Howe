72a32aba050389ec15630775dcd26efff7c09892
salkeith <sally.a.keith@gmail.com>
2014-08-21 16:09:23 +0200

    Added code to calculate relative abundance, re-join with lagoon data, run ANOSIM, run NMDS

--- LHvsGBRBootstrap.R
+++ LHvsGBRBootstrap.R
@@ -73,6 +73,124 @@ for(z in 1:bootstrap.n){
    boot.d[[z]] <- subset(d,d$id %in% boot.id[,,z])
 }
 
+# order transects alphabetically so ensure they match up in other bits of code
+boot.d <- lapply(boot.d,function(x) x[order(x$id),])
+# simplify boot.id into 2D array
+boot.id2 <- matrix(NA,54,bootstrap.n)
+for(z in 1:bootstrap.n){
+   boot.id2[,z] <- sort(as.vector(t(boot.id[,,z])))
+}
+boot.id <- t(boot.id2)
+
+save(boot.d,boot.id,file="BootstrappedData.RData")
+
+
+
+#######################################################
+#######################################################
+
+## CALCULATE RELATIVE ABUNDANCE
+
+load("BootstrappedData.RData")
+
+boot.mat <- list()
+for(z in 1:bootstrap.n){
+   # create empty vector to hold result of loop
+   cover.relative.bs <- c()
+   # calculate relative cover (i.e., proportion of total coral cover)
+      for(tr in 1:(sample.n*9)){
+         x <- subset(boot.d[[z]],boot.d[[z]]$id==boot.id[z,tr])
+         cover.relative.bs <- c(cover.relative.bs,x$cover.cm/sum(x$cover.cm))
+      }
+   boot.d[[z]] <- cbind(boot.d[[z]],cover.relative.bs)
+
+}
+# check data looks right
+head(boot.d[[1]])
+
+
+
+#######################################################
+#######################################################
+
+## JOIN BOOTSTRAPPED CREST DATA BACK WITH ORIGINAL 
+## LAGOON DATA & FORMAT INTO SITE BY SPECIES MATRIX
+
+
+# create list to hold output
+boot.join <- list()
+# loop through bootstrapped data frames
+for(z in 1:bootstrap.n){
+   # add new column so it will bind with bootstrapped data
+   d$cover.relative.bs <- d$cover.relative
+   boot.join[[z]] <- rbind(subset(d,d$habitat=="lagoon"),boot.d[[z]])
+   # format as site by species matrix
+   boot.mat[[z]] <- cast(boot.join[[z]],species~id,value="cover.relative.bs")
+   rownames(boot.mat[[z]]) <- boot.mat[[z]][,1]
+   boot.mat[[z]] <- boot.mat[[z]][,-1]
+}
+# check data looks right
+boot.mat[[1]][1:30,1:10]
+dim(boot.mat[[1]])
+
+
+save(boot.mat,boot.join,boot.d,boot.id,file="BootstrappedData.RData")
+
+
+
+#######################################################
+#######################################################
+
+## RUN ANOSIM & NMDS ON BOOTSTRAPPED DATA (i.e., x 1000)
+## GET MEAN AND CONFIDENCE INTERVALS OF AXIS SCORES
+
+habitat.short <- rep(c("lagoon","crest"),c(108,54))
+island.short <- rep(c("lizard","one.tree","lord.howe","lizard","one.tree","lord.howe"),c(36,36,36,18,18,18))
+region <- rep(c("GBR","lord.howe","GBR","lord.howe"),c(72,36,36,18))
+
+empty.sp <- c()  # empty vector to record absent species 
+boot.anosim <- matrix(NA,bootstrap.n,6)
+for(z in 1:bootstrap.n){
+   if(z %in% seq(100,1000,100)) print(z)
+   # remove species with no records (to make ANOSIM work)
+   x <- subset(boot.mat[[z]],rowSums(boot.mat[[z]])>0)
+   # record number of species absent in each data set
+   empty.sp[z] <- nrow(boot.mat[[z]]-nrow(x))
+   # ANOSIM
+   x <- t(x)
+   a.island <- anosim(x,island.short,permutations=1000,distance="bray")
+   boot.anosim[z,1] <- a.island$statistic
+   boot.anosim[z,2] <- a.island$signif
+   a.region <- anosim(x,region,permutations=1000,distance="bray")
+   boot.anosim[z,3] <- a.region$statistic
+   boot.anosim[z,4] <- a.region$signif
+   a.habitat <- anosim(x,habitat.short,permutations=1000,distance="bray")
+   boot.anosim[z,5] <- a.habitat$statistic
+   boot.anosim[z,6] <- a.habitat$signif
+}
+# check the data look right
+head(boot.anosim)
+summary(empty.sp)
+
+# might need to use mean rank of score rather actual score
+# because axes might look totally different for each MDS
+boot.mds.sites <- list()
+boot.mds.sp <- list()
+mds.stress <- c()  # record stress value for each MDS
+
+for(z in 1:bootstrap.n){
+   if(z %in% seq(100,1000,100)) print(z)
+   x <- subset(boot.mat[[z]],rowSums(boot.mat[[z]])>0)
+   x <- t(x)
+   mds <- metaMDS(x,trymax=50)
+   mds.stress[z] <- round(mds$stress,3)   
+   boot.mds.sites[[z]] <- mds$points
+   boot.mds.sp[[z]] <- mds$species
+}
+
+
+save(boot.anisim,empty.sp,mds.stress,boot.mds.sites,boot.mds.sp,
+        file="BootstrappedANOSIMandMDS.RData")
 
 
 
